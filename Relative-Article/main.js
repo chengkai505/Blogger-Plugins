64fe61f90e3f5cb9f292b89aab3a4453480b9f74
Kai Wu <wuvincentck505@gmail.com>
2020-11-03 22:05:48 +0800

    Migrate

---
+++ Relative-Article/main.js
@@ -0,0 +1,83 @@
+let ra = {};
+ra.domList = document.getElementById("related-article");
+ra.data = new Array;
+ra.max = 8;
+ra.noImageAvailable = "//1.bp.blogspot.com/-NkizAITi-W8/XiQMhLmaFSI/AAAAAAAAZz0/JJ84CNmiFl0r_As8yUj5-w03rBabBMwHwCKgBGAsYHg/s90-c-rj-e30/noImg.jpg";
+if (ra.domList) {
+	ra.fragment = document.createDocumentFragment();
+	ra.fragment.appendChild(cssNode("//chengkai505.github.io/Blogger-Plugins/Relative-Article/style.css"));
+	ra.url = ra.dataset.url;
+	ra.label = ra.dataset.label;
+	document.head.appendChild(scriptNode("https://kai73002981.blogspot.com/feeds/posts/default/-/" + ra.label + "?alt=json-in-script&callback=ra_input&max-results=" + ra.max * 2, function () {
+		let i = ra.max < ra.data.length ? ra.max : ra.data.length,
+			m = new Array;
+		for (let s = 0; m.push(s++) < ra.data.length;);
+		if (m = m.sort(function () { return Math.random() - .5 }), ra.data.length <= 1) {
+			let p = document.createElement("p");
+			p.innerText = "沒有相關文章";
+			ra.fragment.appendChild(p);
+		} else
+			for (let p = 0; p < i; p++) {
+				let data = ra.data[m[p]];
+				if (ra.url != data.link) {
+					let post = document.createElement("div"),
+						title = document.createElement("p"),
+						thumbnail = document.createElement("div"),
+						img = document.createElement("img"),
+						link = document.createElement("a");
+					post.classList.add("post");
+					thumbnail.classList.add("thumbnail");
+					img.src = data.thumbnail;
+					img.alt = data.title;
+					title.innerText = data.title;
+					link.title = data.title;
+					link.href = data.link;
+					thumbnail.appendChild(img);
+					post.appendChild(thumbnail);
+					post.appendChild(title);
+					post.appendChild(link);
+					ra.fragment.appendChild(post);
+				}
+			}
+		ra.appendChild(ra.fragment);
+	}));
+}
+function ra_input(e) {
+	for (let t = 0; t < e.feed.entry.length; t++) {
+		let r = new Object,
+			n = e.feed.entry[t];
+		r.title = n.title.$t;
+		try {
+			r.thumbnail = n.media$thumbnail.url;
+		} catch (e) {
+			r.thumbnail = ra.noImageAvailable;
+		}
+		if (r.thumbnail.includes(".bp.blogspot.com")) {
+			let a = r.thumbnail.split("/");
+			a[7] = "s90-c-e30";
+			r.thumbnail = a.join("/");
+		}
+		for (let l = 0; l < n.link.length; l++)
+			if ("alternate" == n.link[l].rel) {
+				r.link = n.link[l].href;
+				break
+			} for (var i = 0, m = 0; m < ra.data.length; m++)
+			if (r.link == ra.data[m].link) {
+				i = 1;
+				break
+			} i || ra.data.push(r)
+	}
+}
+function scriptNode(a, c = 0) {
+	let s = document.createElement("script");
+	s.src = a;
+	s.asunc = true;
+	c != 0 ? s.onload = c : 0;
+	return s;
+}
+function cssNode(a) {
+	let l = document.createElement("link");
+	l.href = a;
+	l.rel = "stylesheet";
+	return l;
+}
