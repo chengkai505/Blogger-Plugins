02da5f174c2a4fbec4cd3523042ad2b486f7419b
Kai Wu <wuvincentck505@gmail.com>
2020-11-06 23:05:26 +0800

    add lazyload

---
+++ Image-Lazyload/main.js
@@ -0,0 +1,27 @@
+let lazyload = {};
+
+lazyload.dom = document.getElementsByClassName("lazyload");
+if (lazyload.dom.length > 0) lazyload.main();
+
+lazyload.main = function() {
+	let observer = new IntersectionObserver(function (entries, observer) {
+		entries.forEach(function (entry) {
+			if (entry.isIntersecting) {
+				let img = entry.children[0];
+				img.classList.add("loading");
+				if (img.complete) {
+					img.classList.add("loaded");
+				} else {
+					img.onload = function () {
+						img.classList.add("loaded");
+					}
+				}
+				img.src = img.dataset.src;
+				observer.unobserve(entry.target);
+			}
+		});
+	});
+	for (let i = 0; i < lazyload.dom.length; i++) {
+		observer.observe(lazyload.dom[i]);
+	}
+};
